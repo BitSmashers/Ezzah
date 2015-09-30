0ee92798a4694eecd5590326c9cb0f58769669f4
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-09-30 17:00:31 +0200

    Add db unsecure connection

--- core/src/main/main.go
+++ core/src/main/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"log"
 	"io/ioutil"
+	"github.com/jmcvetta/neoism"
 	"github.com/daaku/go.httpgzip"
 )
 
@@ -47,6 +48,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		- Ezzah server (http handlers)
  */
 func startServer() {
+	log.Println("Setting up server handlers")
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/", ezzahHandler)
 	serveMux.HandleFunc("/search", searchHandler)
@@ -64,7 +66,28 @@ func initEnv(){
 
 func initDatabase() {
 	log.Println("Trying to connect database...")
+	//http://your_user:your_password@neo4j.yourdomain.com/db/data/
+	db, err := neoism.Connect("http://neo4j:unsecure@localhost:7474/db/data")
+	n, err := db.CreateNode(neoism.Props{"name": "Captain Kirk"})
+	defer n.Delete()  // Deferred clean up
+	n.AddLabel("Person") // Add a label
 
+	res := []struct {
+		// `json:` tags matches column names in query
+		A   string `json:"a.name"`
+	}{}
+	cq := neoism.CypherQuery{
+		// Use backticks for long statements - Cypher is whitespace indifferent
+		Statement: `MATCH (a { name: "Captain Kirk"}) RETURN a LIMIT 100`,
+		Parameters: neoism.Props{"name": "Dr McCoy"},
+		Result:     &res,
+	}
+	err = db.Cypher(&cq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(db,n)
+	log.Println("Result => ", cq.Result) // Dont know yet how to tame results :)
 }
 
 func initBaseDbMock() {
--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source env.toSource
+source ./env.toSource
 
 # Check environment
 if [ "$NEODB_PATH" = "" ];
@@ -13,10 +13,18 @@ fi
 
 # Go dependencies
 echo "Downloading go dependencies...."
+
 echo "go-json-rest from github.com/ant0ine/go-json-rest/rest"
-go get github.com/ant0ine/go-json-rest/rest
+go get -v github.com/ant0ine/go-json-rest/rest
+
 echo "httpgzip from github.com/daaku/go.httpgzip"
-go get github.com/daaku/go.httpgzip
+go get -v github.com/daaku/go.httpgzip
+
+echo "sqlx from github.com/jmoiron/sqlx"
+go get -v github.com/jmoiron/sqlx
+
+echo "cypher driver (neoism) from github.com/jmcvetta/neoism"
+go get -v github.com/jmcvetta/neoism
 echo "Go dependencies updated"
 
 # Run Ezzah binary
