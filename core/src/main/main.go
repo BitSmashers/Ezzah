9fb4588b99046102c91ecfc4ae8e58ef910014e6
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-09-30 15:31:44 +0200

    Main structures db connection wip

--- core/src/main/main.go
+++ core/src/main/main.go
@@ -2,23 +2,71 @@
 package main
 
 import (
-//	"fmt"
+	"os"
 	"net/http"
 	"log"
-//	"html"
+	"io/ioutil"
+	"github.com/daaku/go.httpgzip"
 )
 
+var neo4jURL string
+
 func main() {
+	initEnv()
+	initDatabase()
+	initBaseDbMock()
 	startServer()
+	log.Println("Db URL : ", neo4jURL)
 }
+
+/*
+	Handlers
+*/
 
 func ezzahHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("form ", r.Form, " body", r.Body, " url ", r.URL," postVal(\"user\") ", r.PostFormValue("user"))
+	log.Println("form ", r.Form, " body", r.Body, " url ", r.URL, " postVal(\"user\") ", r.PostFormValue("user"))
 	w.Write([]byte("<h1>Ezzah</h1>"))
+
+	w.Header().Set("Content-Type", "text/html")
+	body, _ := ioutil.ReadFile("public/index.html")
+	w.Write(body)
 }
 
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+
+
+	w.Header().Set("Content-Type", "text/html")
+	body, _ := ioutil.ReadFile("public/index.html")
+	w.Write(body)
+}
+
+/*
+	Init functions
+		- Db
+		- Mock data
+		- Ezzah server (http handlers)
+ */
 func startServer() {
-	http.HandleFunc("/bar", ezzahHandler)
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", ezzahHandler)
+	serveMux.HandleFunc("/search", searchHandler)
+
+	panic(http.ListenAndServe(":" + os.Getenv("PORT"), httpgzip.NewHandler(serveMux)))
 }
 
+func initEnv(){
+	if os.Getenv("GRAPHENEDB_URL") != "" {
+		neo4jURL = os.Getenv("GRAPHENEDB_URL")
+	}else{
+		neo4jURL = "http://localhost:7474" // Default config provided by neo4J
+	}
+}
+
+func initDatabase() {
+	log.Println("Trying to connect database...")
+
+}
+
+func initBaseDbMock() {
+	log.Println("Inserting mock data...")
+}
