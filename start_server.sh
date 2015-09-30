312e7ffaa524ab079c400bbc1b308aefa953b4b9
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-09-30 15:38:51 +0200

    Scripts launch Ezzah Binary

--- start_server.sh
+++ start_server.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
+source env.toSource
 
+# Check environment
 if [ "$NEODB_PATH" = "" ];
   then
 	echo "environment var NEODB_PATH is not set, please see env.toSource file."
 	exit -1
   else
-	echo "Starting Neo4J instance..."
+	echo "Starting Neo4J instance... $NEODB_PATH"
 fi
 
+# Go dependencies
 echo "Downloading go dependencies...."
 echo "go-json-rest from github.com/ant0ine/go-json-rest/rest"
 go get github.com/ant0ine/go-json-rest/rest
+echo "httpgzip from github.com/daaku/go.httpgzip"
+go get github.com/daaku/go.httpgzip
 echo "Go dependencies updated"
 
-
-
+# Run Ezzah binary
+go run core/src/main/main.go
