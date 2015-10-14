7a9fc9b80a3dac62a945a4ed6cd331f3b4d39023
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-10-14 15:54:46 +0200

    Verbose mode for go get

--- start_server.sh
+++ start_server.sh
@@ -12,16 +12,16 @@ cd core
 echo "Downloading go dependencies...."
 
 echo "go-json-rest from github.com/ant0ine/go-json-rest/rest"
-go get github.com/ant0ine/go-json-rest/rest
+go get -v github.com/ant0ine/go-json-rest/rest
 
 echo "httpgzip from github.com/daaku/go.httpgzip"
-go get github.com/daaku/go.httpgzip
+go get -v github.com/daaku/go.httpgzip
 
 echo "sqlx from github.com/jmoiron/sqlx"
-go get github.com/jmoiron/sqlx
+go get -v github.com/jmoiron/sqlx
 
 echo "cypher driver (neoism) from github.com/jmcvetta/neoism"
-go get github.com/jmcvetta/neoism
+go get -v github.com/jmcvetta/neoism
 
 echo "Go dependencies updated"
 
