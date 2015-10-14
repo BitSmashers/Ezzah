2b503cb7ec83a23b3c72c78a0aaf10b6461e6e39
Adrien Maillol <adrien.maillol@density-tech.com>
2015-10-14 12:02:53 +0200

    fix travis build

--- start_server.sh
+++ start_server.sh
@@ -6,24 +6,9 @@ if [ -e ./env.toSource.local ]
 	source ./env.toSource.local
 fi
 
+sh ./godeps.sh
+
 cd core
-
-# Go dependencies
-echo "Downloading go dependencies...."
-
-echo "go-json-rest from github.com/ant0ine/go-json-rest/rest"
-go get github.com/ant0ine/go-json-rest/rest
-
-echo "httpgzip from github.com/daaku/go.httpgzip"
-go get github.com/daaku/go.httpgzip
-
-echo "sqlx from github.com/jmoiron/sqlx"
-go get github.com/jmoiron/sqlx
-
-echo "cypher driver (neoism) from github.com/jmcvetta/neoism"
-go get github.com/jmcvetta/neoism
-
-echo "Go dependencies updated"
 
 # Check environment
 if [ "$NEODB_PATH" = "" ];
