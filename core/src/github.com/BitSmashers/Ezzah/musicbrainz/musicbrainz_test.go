96c3a9607930bba670c8ca31626252ce397a8944
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-10-24 23:03:57 +0200

    New test musicbrainz

--- core/src/github.com/BitSmashers/Ezzah/musicbrainz/musicbrainz_test.go
+++ core/src/github.com/BitSmashers/Ezzah/musicbrainz/musicbrainz_test.go
@@ -1,1 +1,15 @@
-package musicbrainz_test
+package musicbrainz
+
+import (
+	"testing"
+	"log"
+)
+
+func TestSearchArtist(t *testing.T) {
+	artists := ArtistSearch("Eminem")
+
+	for _, a := range artists {
+		log.Println("Artists : ", a.Name)
+	}
+
+}
